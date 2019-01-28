f1374674f17611d3b89ccf4aa26080d258ce5f01
wiper0994 <32376640+wiper0994@users.noreply.github.com>
2019-01-28 10:53:19 -0500

    Add files via upload
    Horario

---
+++ 44025.php
@@ -0,0 +1,95 @@
+<html><head>
+<title>Sistema de Registro FUKL</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="expires">
+<link rel="stylesheet" type="text/css" href="StyleSheet.css">
+
+<script language="JavaScript">
+<!-- 
+function openPop(pagename){
+    window.open(pagename,'','toolbar=no,scrollbars=yes,location=no,directories=no,status=no,menubar=no,resizable=yes,height=420,width=740')} 
+//--> 
+</script>
+</head>
+
+<body text="#000000" link="#070785" vlink="#070785">
+    <table border="0" cellpadding="0" align="center" cellspacing="0">
+    <tbody><tr> 
+          <td height="30" align="left" valign="top" width="40%"> 
+            <div align="left"><font face="Arial, Helvetica, sans-serif"><b>
+                <img src="escudoNew.JPG" width="260" height="90"></b></font></div>
+          </td>
+          <td height="30" width="60%"> 
+            <div align="center"><h2>Sistema para la Gestión Curricular - Escolaris</h2></div>  
+          </td>
+    </tr>
+    </tbody></table>
+    <p align="center"><a href="JavaScript:openPop('GuiaEstudiantes')">Ayuda</a></p>
+
+ 
+
+
+
+
+
+
+
+<div align="center"><font face="Arial, Helvetica, sans-serif" size="2" color="#1E5E1E">
+      <table width="80%" border="1" align="center">
+        <tbody><tr> 
+          <td height="6"> 
+              Código: 713161001 &nbsp;&nbsp;Estudiante: PULIDO PULIDO STEFANNY GISEL &nbsp;&nbsp; Semestre: 7 
+          </td>
+        </tr>
+        <tr> 
+          <td height="6"> 
+              Programa: Administracion de Negocios Internacionales Plan 1013 &nbsp;&nbsp; Estado: Estudiante regular &nbsp;&nbsp; 
+              CargaMax: 5
+          </td>
+        </tr> 
+      </tbody></table>
+    </font>
+</div>
+<br>
+<b><center>Información de la Asignatura:Matemática Financiera - 44025
+<b><center><u>Información de Historia Académica de la Asignatura</u></center></b><br>
+
+<table border="1" width="100%">
+<tbody><tr>
+<td><font face="Arial, Helvetica, sans-serif" size="1">Asignatura</font></td>
+<td><font face="Arial, Helvetica, sans-serif" size="1">Periodo</font></td>
+<td><font face="Arial, Helvetica, sans-serif" size="1">Nota</font></td>
+<td><font face="Arial, Helvetica, sans-serif" size="1">Estado</font></td>
+</tr>
+
+<tr>
+</tr><tr>
+<td><font face="Arial, Helvetica, sans-serif" size="1">Matemática Financiera - 44025</font></td>
+<td><font face="Arial, Helvetica, sans-serif" size="1">2017-1</font></td>
+
+    <td><font face="Arial, Helvetica, sans-serif" size="1">32</font></td>
+
+<td><font face="Arial, Helvetica, sans-serif" size="1">Normal</font></td>
+</tr>
+
+
+</tbody></table>
+<center>
+<center><u><br>Requisitos de la Asignatura en el plan de Estudios</u><br>
+
+
+<font face="Arial, Helvetica, sans-serif" size="1">Costos y Presupuestos - </font>
+
+    <font face="Arial, Helvetica, sans-serif" size="1">Pre-Requisito</font><br>
+
+</center>
+<br>
+<form>
+<center><input type="button" value="Close" onclick="window.close()">
+</center>
+</form>
+
+
+
+</center></center></b></body></html>
